7e2c9554061d66db4f909905882cbef7f5b32fd6
Callum Alton <callum.alton33@gmail.com>
2020-05-12 16:59:41 +0100

    added CSS (no js yet)

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const wrapper = document.getElementsByClassName('wrapper')
 const rollDice = document.getElementById('roll')
-
+const playerScore = document.getElementsByClassName('playerScore')
 
 rollDice.onclick = () => {
-    let numberOnDice = (Math.ceil((Math.random() * 5) + 1))
-    console.log(`you rolled a ${numberOnDice}`)
+    let numberOnDice = Math.ceil((Math.random() * 5) + 1)
+    console.log(`${numberOnDice}`)
 }
