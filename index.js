835049eb496524d2a2838ccaa4674b2259b27f7f
Callum Alton <callum.alton33@gmail.com>
2020-05-15 11:36:50 +0100

    added game rules

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const roll = document.getElementById("roll")
 const roll2 = document.getElementById("roll2")
 const newGame = document.getElementById("new-game")
-let player = document.getElementById("player");
-let score = document.getElementById("score");
-let image = document.getElementById("dice-image");
-let player2 = document.getElementById("player2");
-let score2 = document.getElementById("score2");
+let player = document.getElementById("player")
+let score = document.getElementById("score")
+let image = document.getElementById("dice-image")
+let player2 = document.getElementById("player2")
+let score2 = document.getElementById("score2")
 let gameRule = document.getElementById("game-rule")
 
 image.style.visibility = ('hidden') // hides dice at the start
-
+gameRule.innerHTML = "FIRST TO 21 WINS"
+player.innerHTML = "Player 1"
+player2.innerHTML = "Player 2"
+score.innerHTML = 0
+ score2.innerHTML = 0
 // New Game Button
 
 newGame.addEventListener("click", () => {
@@ -20,6 +24,7 @@ newGame.addEventListener("click", () => {
     totalScore2 = 0
     player.innerHTML = "Player 1"
     player2.innerHTML = "Player 2"
+    gameRule.innerHTML = "FIRST TO 21 WINS"
 })
 
 // Player 1 Button
@@ -29,6 +34,7 @@ let totalScore = 0
 roll.onclick = () => {
     player.innerHTML = "Player 1"
     score.innerHTML = 0
+    gameRule.innerHTML = "FIRST TO 21 WINS"
 
     let numberOnDice = Math.ceil(Math.random(1)* 6)
     console.log(`${numberOnDice}`)
@@ -64,9 +70,10 @@ roll.onclick = () => {
 
     score.innerHTML = totalScore
 
-    if (totalScore >= 30) {
+    if (totalScore >= 21) {
     player.innerHTML = ('Winner!')
     player2.innerHTML = ('Loser!')
+    gameRule.innerHTML = ('PLAYER 1 WINS')
     totalScore = 0
     totalScore2 = 0
     }
@@ -80,6 +87,7 @@ let totalScore2 = 0
 roll2.onclick = () => {
     player2.innerHTML = "Player 1"
     score2.innerHTML = 0
+    gameRule.innerHTML = "FIRST TO 21 WINS"
 
     let numberOnDice2 = Math.ceil(Math.random(1)* 6)
     console.log(`${numberOnDice2}`)
@@ -115,9 +123,10 @@ roll2.onclick = () => {
 
     score2.innerHTML = totalScore2
 
-    if (totalScore2 >= 30) {
+    if (totalScore2 >= 21) {
     player2.innerHTML = ('Winner!')
     player.innerHTML = ('Loser!')
+    gameRule.innerHTML = ('PLAYER 2 WINS')
     totalScore2 = 0
     totalScore = 0
     }
